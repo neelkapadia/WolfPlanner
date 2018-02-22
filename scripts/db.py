534f57b4e57034a1b82ade8b69a1c876ca7a1513
neelkapadia <neel.kapadia10@gmail.com>
2018-02-22 16:04:02 -0500

    Date time issue resolved

--- scripts/db.py
+++ scripts/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-# from datetime import datetime
+from datetime import datetime
 import json
 from pprint import pprint
 from bson import json_util
@@ -12,22 +12,21 @@ from bson import json_util
 def insert(db):
 	json_details = json.dumps(
 		{
-<<<<<<< HEAD
-				'_id': 200,
+				'_id': "rtnaik",
 				'name': "Rohit Naik",
 				'tasks': [
 					{
-						'name': 'STDM HW 1',
+						'name': 'STDM HW 3',
 						'type': 'HW',
-						'duration': 6,
-						'deadline': datetime(2018, 2, 15, 0, 0, 0),
+						'duration': 10,
+						'deadline': datetime(2018, 3, 10, 23, 59, 0),
 						'number': 1
 					},
 					{
-						'name': 'DBMS HW 1',
+						'name': 'SE Proj 1',
 						'type': 'HW',
-						'duration': 4,
-						'deadline': datetime(2018, 2, 12, 0, 0, 0),
+						'duration': 40,
+						'deadline': datetime(2018, 3, 12, 8, 30, 0),
 						'number': 1
 					}
 				],
@@ -36,104 +35,46 @@ def insert(db):
 						'name': "SE",
 						'_id': "CSC510",
 						'startTime': datetime(1, 1, 1, 18, 00, 00),
-						'endTime': datetime(1, 1, 1, 19, 15, 00)
+						'endTime': datetime(1, 1, 1, 19, 15, 00),
 						'days': ["tuesday", "thursday"]
 					},
 					{
 						'name': "DBMS",
 						'_id': "CSC540",
 						'startTime': datetime(1, 1, 1, 13, 30, 00),
-						'endTime': datetime(1, 1, 1, 4, 45, 00)
+						'endTime': datetime(1, 1, 1, 4, 45, 00),
 						'days': ["monday", "wednesday"]
 					},
 					{
 						'name': "STDM",
 						'_id': "CSC591-013",
 						'startTime': datetime(1, 1, 1, 10, 15, 00),
-						'endTime': datetime(1, 1, 1, 11, 30, 00)
+						'endTime': datetime(1, 1, 1, 11, 30, 00),
 						'days': ["monday", "wednesday"]
 					}
 				]
-			}, default = myconverter)
-
-=======
-			'_id': 200199811,
-			'name': "Neel Kapadia",
-			'tasks': [
-				{
-					'name': 'STDM HW 1',
-					'type': 'HW',
-					'duration': 6,
-					# 'deadline': datetime(2018, 2, 15, 0, 0, 0),
-					'number': 1
-				},
-				{
-					'name': 'DBMS HW 1',
-					'type': 'HW',
-					'duration': 4,
-					# 'deadline': datetime(2018, 2, 12, 0, 0, 0),
-					'number': 1
-				}
-			],
-			'fixedTasks': [
-				{
-					'name': "SE",
-					'_id': "CSC510",
-					# 'startTime': datetime(1, 1, 1, 18, 00, 00),
-					# 'endTime': datetime(1, 1, 1, 19, 15, 00)
-					'days': ["tuesday", "thursday"]
-				},
-				{
-					'name': "DBMS",
-					'_id': "CSC540",
-					# 'startTime': datetime(1, 1, 1, 13, 30, 00),
-					# 'endTime': datetime(1, 1, 1, 4, 45, 00)
-					'days': ["monday", "wednesday"]
-				},
-				{
-					'name': "STDM",
-					'_id': "CSC591-013",
-					# 'startTime': datetime(1, 1, 1, 10, 15, 00),
-					# 'endTime': datetime(1, 1, 1, 11, 30, 00)
-					'days': ["monday", "wednesday"]
-				}
-			]
-		}, default=json_util.default)
->>>>>>> 58740e611133832e97f6d75a73794b90235b819e
-
+			}, default=myconverter)
 	d = json.loads(json_details)
 	db.student.insert_one(d)
 
 
 def retrieve(db, id):
-	# print("Hi")
 	query = json.dumps({
 		'_id': id})
 	query = json.loads(query)
 
 	d = db.student.find(query)
 	for doc in d:
-<<<<<<< HEAD
 		print(doc)
 
 def myconverter(o):
     if isinstance(o, datetime):
         return o.__str__()
-=======
-		pprint(doc)
-
->>>>>>> 58740e611133832e97f6d75a73794b90235b819e
 
 client = MongoClient("ds231758.mlab.com", 31758, connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True)
 db = client['se']
 user = "rtrgntsg"
 password = "menzies"
 db.authenticate(user, password)
-<<<<<<< HEAD
-print("Hi")
-#retrieve(db)
 insert(db)
-=======
-# print("Hi")
-retrieve(db, 200199811)
->>>>>>> 58740e611133832e97f6d75a73794b90235b819e
+retrieve(db, "rtnaik")
