f071ac40474996da2e3b6cc82eed40c815d84ebf
neelkapadia <neel.kapadia10@gmail.com>
2018-02-22 14:56:15 -0500

    Date Time issue resolved

--- scripts/db.py
+++ scripts/db.py
@@ -10,21 +10,21 @@ from bson import json_util
 def insert(db):
 	json_details = json.dumps(
 		{
-				'_id': 200199811,
-				'name': "Neel Kapadia",
+				'_id': 200,
+				'name': "Rohit Naik",
 				'tasks': [
 					{
 						'name': 'STDM HW 1',
 						'type': 'HW',
 						'duration': 6,
-						#'deadline': datetime(2018, 2, 15, 0, 0, 0),
+						'deadline': datetime(2018, 2, 15, 0, 0, 0),
 						'number': 1
 					},
 					{
 						'name': 'DBMS HW 1',
 						'type': 'HW',
 						'duration': 4,
-						#'deadline': datetime(2018, 2, 12, 0, 0, 0),
+						'deadline': datetime(2018, 2, 12, 0, 0, 0),
 						'number': 1
 					}
 				],
@@ -32,26 +32,26 @@ def insert(db):
 					{
 						'name': "SE",
 						'_id': "CSC510",
-						#'startTime': datetime(1, 1, 1, 18, 00, 00),
-						#'endTime': datetime(1, 1, 1, 19, 15, 00)
+						'startTime': datetime(1, 1, 1, 18, 00, 00),
+						'endTime': datetime(1, 1, 1, 19, 15, 00)
 						'days': ["tuesday", "thursday"]
 					},
 					{
 						'name': "DBMS",
 						'_id': "CSC540",
-						#'startTime': datetime(1, 1, 1, 13, 30, 00),
-						#'endTime': datetime(1, 1, 1, 4, 45, 00)
+						'startTime': datetime(1, 1, 1, 13, 30, 00),
+						'endTime': datetime(1, 1, 1, 4, 45, 00)
 						'days': ["monday", "wednesday"]
 					},
 					{
 						'name': "STDM",
 						'_id': "CSC591-013",
-						#'startTime': datetime(1, 1, 1, 10, 15, 00),
-						#'endTime': datetime(1, 1, 1, 11, 30, 00)
+						'startTime': datetime(1, 1, 1, 10, 15, 00),
+						'endTime': datetime(1, 1, 1, 11, 30, 00)
 						'days': ["monday", "wednesday"]
 					}
 				]
-			}, default = json_util.default)
+			}, default = myconverter)
 
 
 	d = json.loads(json_details)
@@ -62,12 +62,14 @@ def retrieve(db):
 	query = json.dumps({
     '_id': 200199811})
 	query = json.loads(query)
-	
+
 	d = db.student.find(query)
 	for doc in d:
 		print(doc)
 
-
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
 
 client = MongoClient("ds231758.mlab.com", 31758 ,connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True)
 db = client['se']
@@ -75,4 +77,5 @@ user = "rtrgntsg"
 password = "menzies"
 db.authenticate(user, password)
 print("Hi")
-retrieve(db)
+#retrieve(db)
+insert(db)
