30009d8d586774497b5d9cbc21efbd1ec21cb1fc
neelkapadia <neel.kapadia10@gmail.com>
2018-03-09 19:00:57 -0500

    Updates to main.js

--- controllers/main.js
+++ controllers/main.js
@@ -86,6 +86,41 @@ module.exports = function(controller) {
         // var path = "testpy.py"
         // var spawn = require("child_process").spawn;
         // var pythonProcess = spawn('python',[path, dt]);
+        var path = "scheduling.py";
+        var spawn = require("child_process").spawn;
+        
+        // Login details
+        var unityId = "sgshetty";
+        var slackId = "U90JUGPU1";
+        var email = "sgshetty@ncsu.edu";
+        var name = "Sainag Ganesh Shetty";
+
+        //var n1 = 2;
+        //var n2 = 15;
+        // var day_date = {
+        //     "1": "2018-03-05 20:30:00",
+        //     "2": "2018-03-06 20:30:00",
+        //     "3": "2018-03-07 20:30:00",
+        //     "4": "2018-03-08 20:30:00",
+        //     "5": "2018-03-09 20:30:00",
+        //     "6": "2018-03-10 20:30:00",
+        //     "7": "2018-03-11 20:30:00"
+        // };
+        
+        var buffer_time = 15;
+
+        //var data = [unityId, slackId, email, name];
+
+        //console.log("hii");
+        try{
+        //var pythonProcess = spawn("python",[path, n1, n2, JSON.stringify(day_date)]);
+        var pythonProcess = spawn("python3",[path, unityId, slackId, email, name, JSON.stringify(day_date), buffer_time]);
+        //var pythonProcess = spawn("python",[path, JSON.stringify(data), JSON.stringify(day_date), buffer_time]);
+        }
+        catch(err){
+            console.log(err)
+        }
+
 	});
     controller.hears(['^add task$'], 'direct_message,direct_mention', function(bot, message) {
         console.log("adding task"+message.user)
